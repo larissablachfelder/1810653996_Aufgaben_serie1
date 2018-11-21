f81194f442c64f47b4553b312bdf83182a17b1bb
Larissa Blachfelder <1810653996@stud.fh-kufstein.ac.at>
2018-11-21 17:20:06 +0100

    aufgabe6

--- src/variablenUndAusgaben/main.java
+++ src/variablenUndAusgaben/main.java
@@ -1,5 +1,7 @@
 package variablenUndAusgaben;
 
+import java.sql.SQLOutput;
+
 public class main {
 
     public static void main(String[] args) {
@@ -12,6 +14,9 @@ public class main {
         int alter = 19;
         System.out.println(alter);
 
+
+
+
         //Aufgabe4
 
         String fachhochschuleKufsteinTirol = "Fachhochschule Kufstein Tirol";
@@ -23,6 +28,27 @@ public class main {
         //In Konstante umwandeln und umbenennen
 
         final String FH_KUFSTEIN_TIROL = "Fachhochschule Kufstein Tirol";
+
+
+
+
+        //aufgabe 5
+        final String aufgabe5;
+
+        //ALT+Command+L markiert das unnötige ;
+
+
+        //aufgabe6
+
+        System.out.println("Java verwendet für Strings \"doppelte Anführungszeichen\" und einzelne Anführungszeichen \'c\' um einzelne Zeichen zu definieren.");
+        System.out.println("Durch \\ kann man auch den Backslash beliebig oft ausgeben: \\\\\\\\");
+
+        // \b löscht den vorherigen Buchstaben \n macht eine neue Zeile und \t setzt einen Tabulator
+        System.out.println("Ich verwende den\b Backspace, die Newline\n und den \tTabulator.");
+
+        //der teil vor \r wird überschrieben und \f macht in der Konsole nichts
+        System.out.println("Durch den Wagenrücklauf wird die Zeile zurückgesetzt und der Teil nach dem \r überschreibt die bestehende Zeile. \f ist in der Konsole nicht bemerkbar, da es einen Seitenumbruch erzwingt.");
+
     }
 
 
