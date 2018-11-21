a56f298d634e4d34aa60d22dc8343e03d1ecc770
Larissa Blachfelder <1810653996@stud.fh-kufstein.ac.at>
2018-11-21 16:33:25 +0100

    aufgabe 3

---
+++ src/variablenUndAusgaben/main.java
@@ -0,0 +1,18 @@
+package variablenUndAusgaben;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        //Name in Variable speichern und ausgeben
+        String name = "Larissa Blachfelder";
+        System.out.println(name);
+
+        //Alter in Variable speichern und ausgeben
+        int alter = 19;
+        System.out.println(alter);
+
+    }
+
+
+}
