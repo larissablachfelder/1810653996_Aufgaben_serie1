a7653c7005de53ffcecbb79fadf54237c85ec615
Larissa Blachfelder <1810653996@stud.fh-kufstein.ac.at>
2018-11-21 15:21:13 +0100

    aufgabe2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
-public class Main {
+public class Main { //die Klasse in der die Main methode liegt, welche als erstes ausgeführt bzw. mit der gestartet wird
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Larissa Blachfelder");
-    }
-}
+    public static void main(String[] args) { //starten/öffnen der main-methode
+        System.out.println("Hello World!"); //Hier wird "Hello World!" ausgegeben
+        System.out.println("Larissa Blachfelder"); //hier wird mein Name ausgegeben
+    } //die methode wird geschlossen
+} //die Klasse wird geschlossen
--- src/paket2/aufgabe1.java
+++ src/paket2/aufgabe1.java
@@ -1,15 +1,15 @@
-package paket2;
+package paket2; //das package mit dem Namen paket2 fasst die Klassen zusammen und dient einer besseren Struktur
 
-public class aufgabe1 {
+public class aufgabe1 { //public heißt, dass es öffentlich ist und class ist eine Klasse
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { //öffentliche Mmain-Methode öffnen; das erste was ausgeführt wird
 
-        System.out.println("FH Kufstien Tirol");
+        System.out.println("FH Kufstien Tirol"); // Hier gebe ich die Worte "FH Kufstein Tirol" aus
 
 
 
-    }
+    }//die methode wird geschlossen
 
-}
+}//die klasse wird geschlossen
 
 
